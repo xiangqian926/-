b8a5170834b651d90aea6a21db273c00608a6ff9
xiangqian <2080430686@qq.com>
2024-12-19 03:09:19 +0800

    Give a personalized hello.

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #incude <stdio.h>
+#incude <string.h>
 
 int main(int argc, char **argv)
 {
-printf("Hello world!\n");
+char name[255];
+
+printf("Enter your name:");
+fegets(name, 255, stdin)
+printf("length = %d\n", strlen(name)); /* debug line */
+name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+
+printf("Hello %!\n", name)
 return 0;
 }

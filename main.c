84f21f0eeb224befe12067948bb0032e6a3f6c85
xiangqian <2080430686@qq.com>
2024-12-19 03:18:28 +0800

    intall commit

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
-#incude <stdio.h>
+#include<stdio.h>
 #incude <string.h>
 
 int main(int argc, char **argv)
 {
-char name[255];
+char name[255], last[255];
+printf("Enter your first name:");
+fegets(name, 255, stdin);
+first[strlen(first)-1]= '\0'; /* remove the newline at the end */
 
-printf("Enter your name:");
-fegets(name, 255, stdin)
-printf("length = %d\n", strlen(name)); /* debug line */
-name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+printf("Now enter your last name: "); 
+gets(last); /* buffer overflow? what's that? */
 
-printf("Hello %!\n", name)
+printf("Hello %s %s!\n", first, last)
 return 0;
 }
